f1e273cb538e9ac90e17cff0187e850fa8f623b1
Smokli <kamen.davidkov@gmail.com>
2015-05-27 08:06:22 -0700

    AngularDemo

---
+++ 60MinutesAngular/60MinutesAngular/AngularTest/controllers/SimpleController.js
@@ -0,0 +1,9 @@
+﻿
+function SimpleController($scope) {
+
+    $scope.customers = [
+        { name: 'Pesho', age: 15, city: 'Sofia' },
+        { name: 'Gosho', age: 33, city: 'Plovdiv' },
+        { name: 'Kamen', age: 25, city: 'Varna' }
+    ]
+}
---
+++ 60MinutesAngular/60MinutesAngular/AngularTest/scripts/angularTest.js
@@ -0,0 +1,64 @@
+﻿
+var demoApp = angular.module('demoApp', ['ngRoute']);
+
+var controllers = {};
+
+
+controllers.SimpleController = function ($scope) {
+
+    $scope.customers = [{ name: 'Pesho', city: 'Varna' }, { name: 'Kamen', city: 'Sofia' }];
+
+    $scope.addCustomer = function () {
+        $scope.customers.push({
+            name: $scope.newCustomer.name,
+            city: $scope.newCustomer.city
+        })
+    };
+
+};
+
+
+demoApp.factory('simpleFactory', function () {
+    var customers = [{ name: 'Pesho', city: 'Varna' }, { name: 'Kamen', city: 'Sofia' }];
+
+    var factory = {};
+
+    factory.getCustomers = function () {
+        return customers;
+    }
+
+    return factory;
+});
+
+demoApp.controller('SimpleController', function ($scope, simpleFactory) {
+
+    $scope.customers = [];
+    $scope.customers = simpleFactory.getCustomers();
+
+    $scope.addCustomer = function () {
+        $scope.customers.push({
+            name: $scope.newCustomer.name,
+            city: $scope.newCustomer.city
+        });
+        $scope.newCustomer.name = '';
+        $scope.newCustomer.city = '';
+
+    }
+});
+
+demoApp.config(['$routeProvider',
+    function ($routeProvider) {
+
+    $routeProvider
+        .when('/',
+        {
+            templateUrl: 'View/View1.html',
+            controller: 'SimpleController'
+        })
+        .when('/view2',
+        {
+            templateUrl: "View/View2.html",
+            controller: 'SimpleController'
+        })
+        .otherwise({ redirectTo: '/' });
+}]);
