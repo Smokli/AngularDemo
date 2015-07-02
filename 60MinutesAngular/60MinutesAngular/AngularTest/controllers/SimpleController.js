3695d798c8f4f8db91d80f2b8ca1ace0b8cbbc99
Smokli <kamen.davidkov@gmail.com>
2015-07-02 08:36:44 +0300

    asdasd
    asdasd

--- 60MinutesAngular/60MinutesAngular/AngularTest/controllers/SimpleController.js
+++
@@ -1,9 +0,0 @@
-﻿
-function SimpleController($scope) {
-
-    $scope.customers = [
-        { name: 'Pesho', age: 15, city: 'Sofia' },
-        { name: 'Gosho', age: 33, city: 'Plovdiv' },
-        { name: 'Kamen', age: 25, city: 'Varna' }
-    ]
-}
